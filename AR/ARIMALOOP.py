ff407a5fbf440c79a66d87c24c3d7c8234cf6b87
MZBolatov <mzbolatov@beeline.kz>
2023-04-23 23:34:41 +0600

    ARIMA,MA model

---
+++ AR/ARIMALOOP.py
@@ -0,0 +1,126 @@
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+
+warnings.filterwarnings('ignore')
+
+# read data
+data = pd.read_csv('E:\четвертый курс\Дипломка\DiplomaSeries\AR\AAPL.csv', usecols=['Date', 'Close'])
+# infer the frequency of the data
+
+# convert Date column to datetime object
+data['Date'] = pd.to_datetime(data['Date'])
+
+# set Date column as index
+data.set_index('Date', inplace=True)
+data.plot()
+plt.title('Stock prices', fontsize=20)
+plt.ylabel('Close price')
+plt.show()
+
+
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+
+result = adfuller(data)
+# plot seasonal decomposition
+decomposition = sm.tsa.seasonal_decompose(data, model='additive', period=25)
+trend = decomposition.trend
+seasonal = decomposition.seasonal
+residual = decomposition.resid
+
+fig = decomposition.plot()
+fig.set_size_inches(14, 7)
+plt.show()
+
+# print test statistics
+print('ADF Statistic:', result[0])
+print('p-value:', result[1])
+print('Critical Values:')
+for key, value in result[4].items():
+    print('\t%s: %.3f' % (key, value))
+
+train_data = data.iloc[:-7, :]
+test_data = data.iloc[-7:, :]
+print(train_data.tail())
+print(test_data)
+# plot ACF and PACF
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+plot_acf(data.Close.diff().dropna(), ax=ax1)
+plot_pacf(data.Close.diff().dropna(), ax=ax2)
+plt.show()
+
+# Original Series
+fig, (ax1, ax2, ax3) = plt.subplots(3)
+ax1.plot(data.Close)
+ax1.set_title('Original Series')
+ax1.axes.xaxis.set_visible(False)
+# 1st Differencing
+ax2.plot(data.Close.diff())
+ax2.set_title('1st Order Differencing')
+ax2.axes.xaxis.set_visible(False)
+# 2nd Differencing
+ax3.plot(data.Close.diff().diff())
+ax3.set_title('2nd Order Differencing')
+plt.show()
+
+# initialize variables for storing best model and minimum RMSE
+best_model = None
+best_rmse = np.inf
+
+# loop over different values of p and q to find the best model
+for p in range(2, 25):
+    for q in range(2, 25):
+        for d in range(1, 3):
+            # fit ARIMA model
+            model = ARIMA(data['Close'], order=(p, d, q))
+            model_fit = model.fit()
+
+            # calculate RMSE for test data
+            test_preds = model_fit.predict(start=test_data.index[0], end=test_data.index[-1])
+            rmse_val = rmse(test_preds, test_data['Close'])
+
+            # update best model and minimum RMSE
+            if rmse_val < best_rmse:
+                best_model = model_fit
+                best_rmse = rmse_val
+
+            print(f"ARIMA({p}, {d}, {q}) - RMSE: {rmse_val:.2f}")
+
+# print summary of best model
+print("\nBest Model:")
+print(best_model.summary())
+
+# plot predictions against actual values for test data
+test_preds = best_model.predict(start=test_data.index[0], end=test_data.index[-1])
+plt.figure(figsize=(10, 6))
+plt.plot(test_data.index, test_data['Close'], label='Actual')
+plt.plot(test_data.index, test_preds, label='Predicted')
+plt.title('ARIMA Model - Test Data', fontsize=20)
+plt.xlabel('Date')
+plt.ylabel('Close')
+plt.show()
+# plot predictions against actual values for test data
+
+plt.figure(figsize=(10, 6))
+plt.plot(train_data.index, train_data['Close'], label='Train Data')
+plt.plot(test_preds.index, test_preds, label='Predicted')
+plt.plot(test_data.index, test_data['Close'], label='Test Data')
+plt.title('ARIMA Model - Test Data', fontsize=20)
+plt.xlabel('Date')
+plt.ylabel('Close Price')
+plt.legend()
+plt.show()
+
+for i in range(len(test_preds)):
+    print(f"predicted={test_preds[i]:.6f}, expected={test_data['Close'][i]:.6f}")
+rmse_val = rmse(test_preds, test_data['Close'])
+efficiency = (1 - (rmse_val / test_data['Close'].mean())) * 100
+print("RMSE = {:.2f}, Efficiency = {:.2f}%".format(rmse_val, efficiency))
--- AR/MA.py
+++ AR/MA.py
@@ -1,22 +1,24 @@
 from math import sqrt
+from statistics import mean
 
+import matplotlib.dates as mdates
 import pandas as pd
 import statsmodels.api as sm
 from matplotlib import pyplot
-from pandas.plotting import lag_plot
 from sklearn.metrics import mean_squared_error
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from statsmodels.tsa.ar_model import AutoReg
 
 data = pd.read_csv('E:\четвертый курс\Дипломка\DiplomaSeries\AR\AAPL.csv', usecols=['Date', 'Close'])
+data['Date'] = pd.to_datetime(data['Date'])
 data.set_index('Date', inplace=True)
 print(data.head())
 print(data.tail())
-# plot the raw data
 data.plot()
 pyplot.show()
 
-# seaonal decompose
+
+
+# plot seasonal decomposition
 decomposition = sm.tsa.seasonal_decompose(data, model='additive', period=25)
 trend = decomposition.trend
 seasonal = decomposition.seasonal
@@ -24,92 +26,65 @@ residual = decomposition.resid
 
 fig = decomposition.plot()
 fig.set_size_inches(14, 7)
-for ax in fig.axes:
-    ax.set_xticklabels([])
 pyplot.show()
 
-# plot lag plot
-series = pd.Series(data["Close"])
-lag_plot(series)
-pyplot.show()
-
+# Tail-rolling average transform
+rolling = data.rolling(window=3)
+rolling_mean = rolling.mean()
+print(rolling_mean.head(10))
+# Plot the original data and the rolling mean against each other
+pyplot.plot(data.index, data['Close'], label='Original Data')
+pyplot.plot(rolling_mean.index, rolling_mean['Close'], color='red', label='Rolling Mean')
+pyplot.legend()  # Add a legend to the plot
+pyplot.show()  # Show the plot
 # plot autocorrelation
 fig, (ax1, ax2) = pyplot.subplots(2, 1, figsize=(10, 8))
 plot_acf(data.Close.diff().dropna(), ax=ax1)
 plot_pacf(data.Close.diff().dropna(), ax=ax2)
 pyplot.show()
-
-pyplot.show()
-
-# split dataset
-X = series.values
-train, test = X[1:len(X) - 7], X[len(X) - 7:]
-print("train")
-print(train)
-print("test")
-print(test)
-# train autoregression
-window = 29
-model = AutoReg(train, lags=29)
-model_fit = model.fit()
-coef = model_fit.params
-
-# walk forward over time steps in test
-history = train[len(train) - window:]
-history = [history[i] for i in range(len(history))]
+# prepare situation
+X = data.values
+window = 3
+history = [X[i] for i in range(window)]
+test = [X[i] for i in range(window, len(X))]
 predictions = list()
+# walk forward over time steps in test
 for t in range(len(test)):
     length = len(history)
-    lag = [history[i] for i in range(length - window, length)]
-    yhat = coef[0]
-    for d in range(window):
-        yhat += coef[d + 1] * lag[window - d - 1]
+    yhat = mean([history[i][0] for i in range(length - window, length)])
     obs = test[t]
     predictions.append(yhat)
     history.append(obs)
     print('predicted=%f, expected=%f' % (yhat, obs))
-
-# evaluate forecasts
 rmse = sqrt(mean_squared_error(test, predictions))
 print('Test RMSE: %.3f' % rmse)
 
-# plot forecasts against actual outcomes
-pyplot.plot(data.index[-7:], test, label='Actual')
+# plot
+pyplot.plot(test)
+pyplot.plot(predictions, color='red', label='Predicted')
+pyplot.title('Stock prices', fontsize=20)
+pyplot.ylabel('Close price')
 pyplot.xlabel('Date')
-pyplot.ylabel('Value')
-pyplot.title('Actual Close')
 pyplot.show()
-pyplot.gcf().autofmt_xdate()
-
-pyplot.plot(data.index[-7:], predictions, color='red', label='Predicted')
-pyplot.xlabel('Date')
-pyplot.ylabel('Value')
-pyplot.title('Predicted Close')
+# zoom plot
+pyplot.plot(test[0:100])
+pyplot.plot(predictions[0:100], color='red', label='Predicted')
 pyplot.show()
-pyplot.gcf().autofmt_xdate()
 
-# plot forecasts against actual outcomes
-pyplot.plot(data.index[-7:], test, label='Actual')
-pyplot.plot(data.index[-7:], predictions, color='red', label='Predicted')
-pyplot.legend()
-pyplot.show()
-pyplot.gcf().autofmt_xdate()
-
-# plot forecasts against actual outcomes
+# plot original data up to 7 days before the end of the dataset
 pyplot.plot(data.index[-21:-7], data['Close'][-21:-7], label='Actual')
-pyplot.plot(data.index[-7:], predictions, color='red', label='Predicted')
+
+# plot predictions starting from 7 days before the end of the dataset
+pyplot.plot(data.index[-7:], predictions[-7:], color='red', label='Predicted')
+
 pyplot.legend()
 pyplot.xlabel('Date')
 pyplot.ylabel('Close')
 pyplot.xticks(rotation=-45)
 
 pyplot.show()
-pyplot.gcf().autofmt_xdate()
-
 # Convert test to DataFrame
 test_df = pd.DataFrame(test, index=data.index[-len(test):], columns=['Close'])
-
-# Compute RMSE and efficiency
 rmse_val = sqrt(mean_squared_error(test_df['Close'], predictions))
 efficiency = (1 - (rmse_val / test_df['Close'].mean())) * 100
 print("RMSE = {:.2f}, Efficiency = {:.2f}%".format(rmse_val, efficiency))
